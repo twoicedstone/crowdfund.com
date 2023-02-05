705f781d13faaaa4d68f88bdad07825ba3354da7
Innocent Okafor <twoicedstone@gmail.com>
2023-02-05 10:00:55 +0100

    location.reload() removed

--- crowdfund.js
+++ crowdfund.js
@@ -104,5 +104,4 @@ window.addEventListener("resize", () => {
     document.getElementById("mobile-nav").style.display = "none";
 	document.getElementById("nav-hide").style.display = "none";
 	document.querySelector(".major").style.filter = "none";
-	location.reload();
 })
